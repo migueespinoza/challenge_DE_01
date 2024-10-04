ded1b0a76a2008caf415d65385a16190cca1b314
migueespinoza <migueespinoza.op@gmail.com>
2024-10-04 18:11:22 -0300

    Q1 completed

--- src/q1_time.py
+++ src/q1_time.py
@@ -4,17 +4,18 @@ import pandas as pd
 
 def q1_time(file_path: str) -> List[Tuple[datetime.date, str]]:
     df_tweets_full = pd.read_json(file_path, lines = True)
+    df_tweets_full = df_tweets_full[['id', 'date', 'user']]
     df_tweets_full['date_only'] = df_tweets_full['date'].dt.date
 
-    cantidad_url_por_fecha = df_tweets_full.groupby('date_only')['url'].count().reset_index()
-    top10_fechas = cantidad_url_por_fecha.rename(columns={'url':'count'}).sort_values(by='count', ascending=False).head(10)
+    cantidad_id_por_fecha = df_tweets_full.groupby('date_only')['id'].count().reset_index()
+    top10_fechas = cantidad_id_por_fecha.rename(columns={'id':'count'}).sort_values(by='count', ascending=False).head(10)
     top_fechas = top10_fechas['date_only'].to_list()
 
     df_tweets_top_fechas = df_tweets_full[df_tweets_full['date_only'].isin(top_fechas)]
     df_tweets_top_fechas['username'] = df_tweets_top_fechas['user'].apply(lambda x: x['username'])
 
-    cantidad_tweets_por_fecha_y_user = df_tweets_top_fechas.groupby(by=['date_only', 'username'], sort=False)['url'].count().reset_index().\
-        rename(columns={'url': 'count'})
+    cantidad_tweets_por_fecha_y_user = df_tweets_top_fechas.groupby(by=['date_only', 'username'], sort=False)['id'].count().reset_index().\
+        rename(columns={'id': 'count'})
     
     mayor_cantidad_tweets_de_un_usuario_por_fecha = cantidad_tweets_por_fecha_y_user.\
         groupby(by=['date_only'], sort=False)['count'].agg(['max', 'sum']).reset_index()
