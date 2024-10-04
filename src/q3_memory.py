6998d422379fd03bde17e55e48c753fc1e0cd4a4
migueespinoza <migueespinoza.op@gmail.com>
2024-10-04 19:07:48 -0300

    Q3 completed

--- src/q3_memory.py
+++ src/q3_memory.py
@@ -1,4 +1,26 @@
 from typing import List, Tuple
+import json
+import re
+from itertools import chain
+from collections import Counter
+
+def find_mentioned_users(text):
+    result = re.findall('@(\w+)', text)
+    return result if len(result) > 0 else None
 
 def q3_memory(file_path: str) -> List[Tuple[str, int]]:
-    pass
+    
+    dict_data = {}
+    content_list = []
+    with open(file_path) as json_file:
+        for line in json_file.readlines():
+            json_obj = json.loads(line)
+            content_list.append(json_obj['content'])
+
+    dict_data['content'] = content_list
+    mentioned_users = list(map(find_mentioned_users, dict_data['content']))
+    mentioned_users_filtered = filter(lambda x: x is not None, mentioned_users)
+    mentioned_users_full = chain.from_iterable(mentioned_users_filtered)
+    users_counter = Counter(mentioned_users_full)
+    top_10_user_mentioned = sorted(users_counter.items(), key=lambda x: x[1], reverse=True)[:10]
+    return top_10_user_mentioned
--- src/q3_time.py
+++ src/q3_time.py
@@ -1,4 +1,14 @@
 from typing import List, Tuple
+from aux_functions import read_tweets_json_pandas
 
 def q3_time(file_path: str) -> List[Tuple[str, int]]:
-    pass
+
+    df_tweets = read_tweets_json_pandas(file_path)
+    content = df_tweets['content']
+    mentioned_users = content.str.findall(('@(\w+)'))
+    mentioned_users = mentioned_users.explode().dropna().reset_index(drop=True)
+    top10_mentioned_users = mentioned_users.value_counts().head(10)
+    
+    result = [(x, y) for x, y in zip(top10_mentioned_users.index, top10_mentioned_users.values)]
+
+    return result
